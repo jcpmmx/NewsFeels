b906d8695a0653fb6c5d2efa8db7c13f94d8c2a4
Julián Pérez <jcpmmx@gmail.com>
2018-12-12 20:52:39 -0500

    Setting Heroku up

--- newsfeels/settings/prod.py
+++ newsfeels/settings/prod.py
@@ -1,21 +1,14 @@
 # coding=utf-8
 
 
+import django_heroku
+
 from newsfeels.settings.base import *
 
 DEBUG = False
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ADD_A_PROPER_VALUE_HERE',
-        'USER': 'ADD_A_PROPER_VALUE_HERE',
-        'PASSWORD': 'ADD_A_PROPER_VALUE_HERE',
-        'HOST': 'ADD_A_PROPER_VALUE_HERE',
-        'PORT': 'ADD_A_PROPER_VALUE_HERE',
-    }
-}
-
 # ----------------------------------------------------------------------------------------------------------------------
 # Custom settings, common for PROD env
 # ----------------------------------------------------------------------------------------------------------------------
+
+django_heroku.settings(locals())
